59cd1e93995229797d98872672de5c3ee61ca4e4
ghostFaceKillah <garmulewicz@gmail.com>
2016-04-16 17:39:07 +0200

    Trying to get stuff running

--- deep_q_rl/q_network.py
+++ deep_q_rl/q_network.py
@@ -44,8 +44,8 @@ class DeepQLearner:
         self.clip_delta = clip_delta
         self.freeze_interval = freeze_interval
         self.rng = rng
+        self.RAM_SIZE = 128
 #        self.RAM_SIZE = 128 * 4  # times frame skip
-	self.RAM_SIZE = 128
         np.set_printoptions(threshold='nan')
 
         lasagne.random.set_rng(self.rng)
@@ -95,25 +95,25 @@ class DeepQLearner:
 
         q_vals = lasagne.layers.get_output(self.l_out,
             {
-       #         self.l_in: (states / input_scale),
-                self.l_ram_in: (ram_states / 256.0)
+               self.l_in: (states / input_scale),
+               self.l_ram_in: (ram_states / 256.0)
             }
         )
         
         if self.freeze_interval > 0:
             next_q_vals = lasagne.layers.get_output(self.next_l_out,
             {
-       #           self.l_in: (next_states / input_scale),
-                  self.l_ram_in:(next_ram_states / 256.0)
+                 self.l_in: (next_states / input_scale),
+                 self.l_ram_in:(next_ram_states / 256.0)
             }
-            )
+        )
         else:
             next_q_vals = lasagne.layers.get_output(self.l_out,
                 {
-       #           self.l_in: (next_states / input_scale),
-                  self.l_ram_in:(next_ram_states / 256.0),
+                 self.l_in: (next_states / input_scale),
+                 self.l_ram_in:(next_ram_states / 256.0),
                 }
-                )
+            )
             next_q_vals = theano.gradient.disconnected_grad(next_q_vals)
 
         target = (rewards +
@@ -489,57 +489,6 @@ class DeepQLearner:
 
         return l_out
 
-    def build_big_ram_network(self, input_width, input_height, output_dim,
-                          num_frames, batch_size):
-        """
-        Build a 5-layer network using only the information from the ram.
-        """
-
-        self.l_ram_in = lasagne.layers.InputLayer(
-            shape=(batch_size, self.RAM_SIZE)
-        )
-
-        l_hidden1 = lasagne.layers.DenseLayer(
-            self.l_ram_in,
-            num_units=self.RAM_SIZE,
-            nonlinearity=lasagne.nonlinearities.rectify,
-            W=lasagne.init.HeUniform(),
-            b=lasagne.init.Constant(.1)
-        )
-
-        l_hidden2 = lasagne.layers.DenseLayer(
-            l_hidden1,
-            num_units=self.RAM_SIZE,
-            nonlinearity=lasagne.nonlinearities.rectify,
-            W=lasagne.init.HeUniform(),
-            b=lasagne.init.Constant(.1)
-        )
-
-        l_hidden3 = lasagne.layers.DenseLayer(
-            l_hidden2,
-            num_units=self.RAM_SIZE,
-            nonlinearity=lasagne.nonlinearities.rectify,
-            W=lasagne.init.HeUniform(),
-            b=lasagne.init.Constant(.1)
-        )
-
-        l_hidden4 = lasagne.layers.DenseLayer(
-            l_hidden3,
-            num_units=self.RAM_SIZE,
-            nonlinearity=lasagne.nonlinearities.rectify,
-            W=lasagne.init.HeUniform(),
-            b=lasagne.init.Constant(.1)
-        )
-
-        l_out = lasagne.layers.DenseLayer(
-            l_hidden4,
-            num_units=output_dim,
-            nonlinearity=None,
-            W=lasagne.init.HeUniform(),
-            b=lasagne.init.Constant(.1)
-        )
-
-        return l_out
 
     def build_ram_dropout_network(self, input_width, input_height, output_dim,
             num_frames, batch_size):
